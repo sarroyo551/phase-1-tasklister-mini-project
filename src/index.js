ac07559a36afd62a75851dbe55eecd99e99c0f40
Santi Arroyo <santi.arroyo551@gmail.com>
2022-06-28 13:06:33 -0400

    submitted tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const form = document.getElementById('create-task-form');
+    const tasks = document.getElementById('tasks')
+    const taskDescription = document.getElementById('new-task-description')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const task = document.createElement('li')
+        task.textContent = taskDescription.value
+        tasks.appendChild(task)
+
+        const deleteBtn = document.createElement('button')
+        deleteBtn.textContent = 'x'
+        task.appendChild(deleteBtn)
+
+        deleteBtn.addEventListener('click', () => {
+          task.remove()
+        } )
+
+        taskDescription.value = ''
+    }   )
 });
